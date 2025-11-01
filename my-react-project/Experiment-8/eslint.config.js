b6d287de7d381d582511f9ecfb3dac6d7a28e977
Your Name <23BAI70358@cuchd.in>
2025-11-01 22:28:40 +0530

    Update ESLint to v9.x with proper flat config

--- my-react-project/Experiment-8/eslint.config.js
+++ my-react-project/Experiment-8/eslint.config.js
@@ -1,29 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+export default [{
+  files: ['**/*.{js,jsx}'],
+  ignores: ['dist/**'],
+  plugins: {
+    'react-hooks': reactHooksPlugin,
+    'react-refresh': reactRefreshPlugin
   },
-])
+  languageOptions: {
+    ecmaVersion: 2024,
+    sourceType: 'module',
+    globals: {
+      ...globals.browser,
+      ...globals.es2021
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      }
+    }
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+    'no-console': 'warn'
+  }
+}];
